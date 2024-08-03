985b8c090f80dd24efbaa50e126d002feefc0bad
Aditya Das <aditya1101das@gmail.com>
2024-08-03 21:27:46 +0530

    Exercise6

---
+++ Week_1_Exercises_Part_2/Exercise6/RealImage.java
@@ -0,0 +1,22 @@
+public class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName);
+        try {
+            Thread.sleep(2000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+}
