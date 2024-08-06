69cc0929b57ff36e04fa8612bad845d9b1d08ac0
Aditya Das <aditya1101das@gmail.com>
2024-08-06 10:02:42 +0530

    Week 2 Exercises Part2

---
+++ Week_2_Exercises_Part_2/librarymanagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.displayBooks();
+    }
+}
---
+++ Week_2_Exercises_Part_2/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void getAllBooks() {
+        System.out.println("Fetching all books...");
+    }
+}
